ef0b02568c387942011f64de2426606cf2903c87
Ines Radic <inesradic12314@gmail.com>
2021-02-13 22:13:17 -0800

    update schema

--- schema.sql
+++ schema.sql
@@ -2,31 +2,33 @@ DROP DATABASE IF EXISTS employee_trackerDB;
 
 CREATE DATABASE employee_trackerDB;
 
-CREATE TABLE department (
-    id INT NOT NULL,
-    name VARCHAR(30) NULL,
+USE employee_trackerDB;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL,
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NULL,
-    salary DECIMAL(10, 4) NULL,
+    salary DECIMAL NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-        REFERENCES department(id),
+        REFERENCES departments(id),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL,
     first_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
     role_id INT NOT NULL,
     FOREIGN KEY (role_id)
-        REFERENCES role(id),
-    manager_id INT NOT NULL,
+        REFERENCES roles(id),
+    manager_id INT,
     FOREIGN KEY (manager_id)
-        REFERENCES employee(id),
+        REFERENCES employees(id),
     PRIMARY KEY (id)
 )
