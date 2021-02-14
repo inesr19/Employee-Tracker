0253dc9fc0c0126bdb67a124154273e5a4b151a6
Ines Radic <inesradic12314@gmail.com>
2021-02-13 22:18:48 -0800

    create connection

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const { printTable } = require('console-table-printer');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+    user: 'root',
+
+    password: '',
+    database: 'employee_trackerDB',
+});
